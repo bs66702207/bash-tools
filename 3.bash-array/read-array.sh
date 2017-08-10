bc363afbb5a2ba0aee793e48efc745e8831b2eb5
wujian <wujian@smartisan.com>
2017-08-10 10:54:05 +0800

    Shell array supplement @ *
    Signed-off-by: wujian <wujian@smartisan.com>

--- 3.bash-array/read-array.sh
+++ 3.bash-array/read-array.sh
@@ -10,6 +10,10 @@ echo ${array_name[@]}
 
 expr "1" + "2"
 
+#获取数组中的所有元素
+echo "数组的元素为: ${array_name[*]}"
+echo "数组的元素为: ${array_name[@]}"
+
 #获取数组的长度
 length=${#array_name[@]}
 echo $length
--- README.md
+++ README.md
@@ -6,5 +6,6 @@
 1.bash-variable:
 2.bash-string:
 3.bash-array:
+
 传递参数
 4.bash-argc:
